273cb2516384b2ffa79a8c95b07a831e83dcfd9a
shikder-Klikit <134600032+shikder-klikit@users.noreply.github.com>
2024-04-18 16:31:17 +0600

    menu created & searched & deleted

--- cypress/support/Admin_pages/MenuPage.js
+++ cypress/support/Admin_pages/MenuPage.js
@@ -13,6 +13,7 @@ const currentDate = new Date();
 const formattedDate = `${currentDate.toLocaleString('en-US', { month: 'long' })} ${currentDate.getDate()}`;
 import {faker} from "@faker-js/faker";
 const fakeMenuName = faker.company.buzzNoun();
+//menu section
 Cypress.Commands.add('SelectBusinessForMenu', () =>{
     cy.get('.rounded-lg > .sc-select-container > .sc-selector > .sc-select-placeholder').click()
     cy.get('.sc-select-search').type(testData.data.Menu.BusinessName)
@@ -21,4 +22,38 @@ Cypress.Commands.add('SelectBusinessForMenu', () =>{
 })
 Cypress.Commands.add('AddMenuName',() =>{
     cy.get('.sc-input-container > .border').type(testData.data.Menu.MenuName+' '+fakeMenuName+' '+formattedDate)
+})
+Cypress.Commands.add('AddMenuLogo', () =>{
+    cy.get('.mt-3 > .h-8').selectFile(testData.data.Menu.upload_logo)
+})
+Cypress.Commands.add('AddMenuDescription', () =>{
+    cy.get('.sc-textarea').type(faker.location.streetAddress({useFullAddress: true }))
+})
+Cypress.Commands.add('AddMenuHour', () =>{
+    cy.get('.bg-grey-lighter > .flex').click()
+    cy.get('.mt-4 > .items-center > .text-xs').click()
+    //start time
+    cy.xpath('//div[@class=\'flex flex-col w-full items-start mr-2\']//div[@class=\'sc-timepicker-label border border-red-light\'][normalize-space()=\'00:00\']').click()
+    cy.selectTimeFromPicker('01',0)
+    cy.get('.p-2 > .mt-1').click()
+    //end time
+    cy.xpath('//div[@class=\'flex w-full\']//div[@class=\'flex flex-col w-full items-start\']//div[@class=\'sc-timepicker-label border border-red-light\'][normalize-space()=\'00:00\']').click()
+    cy.selectTimeFromPicker('23',0)
+    cy.get('.p-2 > .mt-1').click()
+})
+Cypress.Commands.add('MenuSaveButton', () =>{
+    cy.get('.menu-tab-content-header-right > .justify-end > .ml-4').click()
+})
+//menu search & delete
+
+Cypress.Commands.add('MenuSearch', () =>{
+    cy.get('.sc-input-container > .h-8').type(testData.data.Menu.MenuName+' '+fakeMenuName+' '+formattedDate)
+})
+Cypress.Commands.add('MenuDelete', () =>{
+    cy.MenuSearch()
+    cy.wait(2000)
+    cy.get('.flex-row.justify-end > .relative > .divide-y > div > .kt-three-dots-vertical').click()
+    cy.get('.flex-row.justify-end > .relative > .action-dropdown > :nth-child(1) > :nth-child(2)').click()
+    cy.get('.sc-modal-visible > .sc-modal-content > .sc-modal-body > .flex-col > .flex > .sc-btn-primary').click()
+
 })
