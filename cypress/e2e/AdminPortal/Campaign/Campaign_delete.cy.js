f5a53378521e39240804223ab0454cc3e72d0a57
Ehsanul Alam Sabbir <easabbir@gmail.com>
2024-05-03 12:12:22 +0600

    Delete Campaign Added

---
+++ cypress/e2e/AdminPortal/Campaign/Campaign_delete.cy.js
@@ -0,0 +1,31 @@
+const login = () => {
+    cy.LoginAsAutomationAdminWithsession()
+}
+describe('Campaign_delete',function() {
+    beforeEach(() => {
+        login('user')
+        cy.VisitHomePage()
+    })
+
+    it('should delete campaign from dashboard.', function () {
+        cy.SelectCampaign();
+        cy.filterCampaignByBusinessName();
+        //cy.filterCampaignByStatus("expired");
+        cy.filterCampaignByStatus("upcoming");
+        cy.filterCampaignByStatus("active");
+
+
+        let totalItemToDelete = cy.get('span:nth-child(4)').invoke('text')
+            .then(itemToDeleteText => {
+                cy.log('item no', itemToDeleteText);
+            })
+
+        cy.deleteCampaign();
+        })
+
+})
+
+
+
+
+
